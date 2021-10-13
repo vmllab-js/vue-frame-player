678ce59085573a363c7ffbbe853c145b35c4278e
Rhine.Liu <rhine@rhine.me>
2021-10-13 19:42:09 +0800

    first version

---
+++ example/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="home">
+		<FramePlayer v-if="config" :config="config"/>
+	</div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import FramePlayer from '@/components/FramePlayer.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      FramePlayer
+    },
+    data() {
+      return {
+        config: null
+      }
+    },
+    mounted() {
+      // let initialImages = [];
+      // for ( let i = 1; i <= 40; ++i ) {
+      //   initialImages.push( `/public/bg/${i}.jpg` );
+      // }
+      this.config = {
+        length: 40,
+        // initialImages
+        initialImages: ( i, length ) => require( `../assets/bg/${i + 1}.jpg` )
+      };
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+	.home {
+		.frame-player {
+			width: 750px;
+			height: 1490px;
+		}
+	}
+</style>
